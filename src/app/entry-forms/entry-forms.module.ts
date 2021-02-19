412301888f0f65e77527526481acdf9c54044d46
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-19 10:26:27 +0530

    done

---
+++ src/app/entry-forms/entry-forms.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EntryFormsRoutingModule } from './entry-forms-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EntryFormsRoutingModule
+  ]
+})
+export class EntryFormsModule { }
