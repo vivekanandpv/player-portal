678b4efecda1dd3609c7257c8b2cdf4cdd50a520
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-19 12:25:51 +0530

    done

--- src/app/entry-forms/entry-forms.module.ts
+++ src/app/entry-forms/entry-forms.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { EntryFormsRoutingModule } from './entry-forms-routing.module';
 import { TFormComponent } from './components/t-form/t-form.component';
 import { RFormComponent } from './components/r-form/r-form.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TFormComponent, RFormComponent],
   imports: [
     CommonModule,
-    EntryFormsRoutingModule
-  ]
+    EntryFormsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class EntryFormsModule { }
+export class EntryFormsModule {}
