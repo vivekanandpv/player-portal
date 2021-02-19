1514666d5743db486bea0104664a8717377c7304
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-19 16:57:32 +0530

    done

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -6,4 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class RestService {
   constructor(private http: HttpClient) {}
+
+  getItems() {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos');
+  }
+
+  createPlayer(formData: any) {
+    return this.http.post('https://reqres.in/api/users', formData);
+  }
+
+  updatePlayer(formData: any, id: number) {
+    return this.http.put(`https://reqres.in/api/users/${id}`, formData);
+  }
+
+  deletePlayer(id: number) {
+    return this.http.delete(`https://reqres.in/api/users/${id}`);
+  }
 }
