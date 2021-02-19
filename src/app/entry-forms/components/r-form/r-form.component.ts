ca65bab5adb4b0f368cb8d50cd69604f9d843e37
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-19 13:01:50 +0530

    done

--- src/app/entry-forms/components/r-form/r-form.component.ts
+++ src/app/entry-forms/components/r-form/r-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-r-form',
   templateUrl: './r-form.component.html',
-  styleUrls: ['./r-form.component.scss']
+  styleUrls: ['./r-form.component.scss'],
 })
 export class RFormComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      name: ['Default Name'],
+      debut: [2021],
+      genre: ['cricket'],
+      isActive: [true],
+      shortBio: ['some bio here...'],
+    });
   }
 
+  ngOnInit(): void {}
 }
