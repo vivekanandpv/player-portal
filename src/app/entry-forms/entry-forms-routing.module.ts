44147cad10df9cf798ceeee93527a3fc9ecdca74
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-19 11:29:21 +0530

    done

--- src/app/entry-forms/entry-forms-routing.module.ts
+++ src/app/entry-forms/entry-forms-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RFormComponent } from './components/r-form/r-form.component';
+import { TFormComponent } from './components/t-form/t-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 't-form',
+    component: TFormComponent,
+  },
+  {
+    path: 'r-form',
+    component: RFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EntryFormsRoutingModule { }
+export class EntryFormsRoutingModule {}
